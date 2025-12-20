b1c5dd7e34071d326fef9170127628616c07ed19
Teodora Vuleta <teodoravuleta2@gmail.com>
2025-12-20 22:40:56 +0100

    Add Docker configuration: compose, dockerfiles, requirements and db init

---
+++ DB/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS task (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    datum DATE NOT NULL,
+    task2do TEXT NOT NULL
+);
