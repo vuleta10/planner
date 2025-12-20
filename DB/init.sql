4935e4831b3177df51cbccdec126d975f6c2cc0f
Teodora Vuleta <teodoravuleta2@gmail.com>
2025-12-20 22:44:42 +0100

    Final cleanup of init.sql before merging to main

--- DB/init.sql
+++ DB/init.sql
@@ -1,12 +1,18 @@
-CREATE TABLE IF NOT EXISTS users (
+-- Kreiranje šeme planner
+CREATE SCHEMA IF NOT EXISTS planner;
+
+-- Kreiranje tabele user unutar šeme planner
+CREATE TABLE IF NOT EXISTS planner."user" (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS task (
+-- Kreiranje tabele task unutar šeme planner
+CREATE TABLE IF NOT EXISTS planner.task (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     datum DATE NOT NULL,
     task2do TEXT NOT NULL
 );
+
